7bdd21d0dc4434b73e034eeb6d3777259f8843bc
Katharina Maier <katharina.a.maier@gmail.com>
2026-01-14 13:27:25 +0100

    fixed paths for image urls

--- components/BookSelectorItem.js
+++ components/BookSelectorItem.js
@@ -1,7 +1,7 @@
 const htmlTemplate = /*html*/`
 <div class="book-selector-item">
     <ul> <!--  die Backslash sind fürs Erkennen der Akzent-Zeichen -->
-      <li><router-link :to="\`/book/\${book.id}\`"><img :src="book.photo ? book.photo : '/img/closed-book-icon.jpg'" alt="book cover" class="book-selector-item-img"></router-link></li>
+      <li><router-link :to="\`/book/\${book.id}\`"><img :src="bookCoverUrl" alt="book cover" class="book-selector-item-img"></router-link></li>
       <li><b>{{ book.title }}</b></li>
       <li>{{ book.author }}</li>
     </ul>
@@ -14,6 +14,12 @@ export default {
     book: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    bookCoverUrl() {
+      let path = this.book.photo ? this.book.photo : '/img/closed-book-icon.jpg';
+      return './' + path;
     }
   }
 };
