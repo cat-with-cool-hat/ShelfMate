ad9ca20913e56ee86c312a054352121cf8efa22c
Katharina Maier <katharina.a.maier@gmail.com>
2026-01-14 16:52:18 +0100

    fixed paths for images

--- components/BookPhotoInput.js
+++ components/BookPhotoInput.js
@@ -3,7 +3,7 @@ const htmlTemplate = /*html*/`
 <div style="text-align: center; margin-bottom: 2em;">
     <b>Upload a photo:</b><br>
     <img v-if="previewSrc" :src="previewSrc" id="preview" alt="preview of your book cover" width="50%"><br> <!-- v-bind:src oder :src -->
-    <img v-if="!previewSrc" src="/img/uploadimage.png" alt="upload book symbol" width="50%"><br>
+    <img v-if="!previewSrc" src="./img/uploadimage.png" alt="upload book symbol" width="50%"><br>
     <input type="file" id="coverphoto" accept="image/*" name="coverphoto" @change="uploadBookPhoto">
 </div>
 `
--- components/LogoBanner.js
+++ components/LogoBanner.js
@@ -1,6 +1,6 @@
 const htmlTemplate = /*html*/`
 <div>
-  <img src="../img/ShelfMate_LogoWithFont.png" alt="a little smiley next to some books and a star above" class="logo-banner">
+  <img src="./img/ShelfMate_LogoWithFont.png" alt="a little smiley next to some books and a star above" class="logo-banner">
 </div>
 `
 
